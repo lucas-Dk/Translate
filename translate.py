1fc4d90ed6bcc9666d2de5fa2656c1ca03c0f2c3
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2020-10-22 01:39:13 -0300

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,63 @@
+# Criando um tradutor de textos
+# Necessário a biblioteca translate
+# Necessário a biblioteca tqdm
+
+
+# Importações
+import os
+import sys
+import time
+from translate import Translator
+from tqdm import tqdm
+
+print("\033[1;31mCarregando tradutor...\033[m")
+for loading in tqdm(range(1, 100 + 1)):
+	time.sleep(0.1)
+print("\033[1;32mTradutor carregado e pronto para uso!\033[m")
+time.sleep(1)
+os.system("clear")
+
+while True:
+	print("""\033[94m
+ _                              _          _         
+| |_   _ _   __ _   _ _    ___ | |  __ _  | |_   ___ 
+|  _| | '_| / _` | | ' \  (_-< | | / _` | |  _| / -_)
+ \__| |_|   \__,_| |_||_| /__/ |_| \__,_|  \__| \___|
+                      
+                      V 1.0
+\033[m\033[1m
+OBS: Nome da linguagem precisa estar em inglês!
+
+exemplo:
+
+traduzir de \033[33mPortuguese\033[m \033[1mpara\033[m \033[1;36mEnglish\033[m
+\033[m""")
+	print("==" * 26 + '\n')
+	try:
+		frase = input("Digite sua frase: ").lower()
+		de = str(input("Traduzir de: ")).capitalize()
+		para = str(input("Para: ")).capitalize()
+	except KeyboardInterrupt:
+		print("\nSaindo...")
+		time.sleep(1)
+		sys.exit()
+	else:
+		traduzir = Translator(from_lang=de,to_lang=para)
+		traducao = traduzir.translate(frase)
+		print("Traduzindo...")
+		time.sleep(0.6)
+		print("\nSua frase: {}".format(frase))
+		print("Tradução: {}".format(traducao))
+		print()
+		again = str(input("Traduzir mais alguma coisa Y/N: ")).upper()
+		while again.strip() not in "Y" and again.strip() not in "N":
+			print("ERROR: Digite uma opção válida para prosseguir...")
+			again = str(input("Traduzir mais alguma coisa Y/N: ")).upper()
+		if again == "S":
+			pass
+		elif again == "N":
+			print("Saindo...")
+			time.sleep(1)
+			sys.exit()
+
+# Fim do script
